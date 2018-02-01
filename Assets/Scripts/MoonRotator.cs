cffdc80a63b7c9539c6f67dd3a184f6cf85b200c
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-01 13:22:10 +0100

    Moon rotator - rotate moon around earth Creating contrainer Planets object with Sun as its child, earth as Sun's child, and moon Earth's child Renaming the base scene towards PlanetarySystem

---
+++ Assets/Scripts/MoonRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotator : MonoBehaviour {
+
+	public float spinSpeed;
+	public float orbitSpeed;
+	private GameObject earthObject;
+
+
+	// Use this for initialization
+	void Start () {
+		earthObject = GameObject.Find("Earth");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+
+		transform.RotateAround (earthObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+	}
+}
