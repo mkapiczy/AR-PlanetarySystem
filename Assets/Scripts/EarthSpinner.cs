096a0eb07cf5069d630e45743de8bb57820381a3
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-01 13:07:07 +0100

    Earth Spinner - rotate around the sun

--- Assets/Scripts/EarthSpinner.cs
+++ Assets/Scripts/EarthSpinner.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthSpinner : MonoBehaviour {
-	public float speed;
+	public float spinSpeed;
+	public float orbitSpeed;
+	private GameObject sunObject;
 
 	// Use this for initialization
 	void Start () {
-		
+		sunObject = GameObject.Find("Sun");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+
+		transform.RotateAround (sunObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
 	}
 }
