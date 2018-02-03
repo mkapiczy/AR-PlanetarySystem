1559dea40bcb4cb930c14417ae380b7d37063a51
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-03 17:58:20 +0100

    Changing image targets - Basic meteor movement and collission detection

--- Assets/Scripts/EarthSpinner.cs
+++ Assets/Scripts/EarthSpinner.cs
@@ -6,17 +6,24 @@ public class EarthSpinner : MonoBehaviour {
 	public float spinSpeed;
 	public float orbitSpeed;
 	private GameObject sunObject;
+	private GameObject meteor;
 
 	// Use this for initialization
 	void Start () {
 		sunObject = GameObject.Find("Sun");
+		meteor = GameObject.Find ("Meteor");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
-
 		transform.RotateAround (sunObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+		Vector3 meteorPosition = meteor.transform.position;
+		Vector3 earthPosition = transform.position;
+
+		if (Vector3.Distance(meteorPosition, earthPosition) <= GetComponent<Renderer>().bounds.size.x) {
+			meteor.SetActive (false);
+		}
 	}
 }
---
+++ Assets/Scripts/MeteorMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorMover : MonoBehaviour {
+
+	private GameObject earth;
+	private GameObject moon;
+	private Vector3 initialPosition;
+	private bool towards = true;
+	private float speed = 0.05f;
+	// Use this for initialization
+	void Start () {
+		earth = GameObject.Find("Earth");
+		moon = GameObject.Find("Moon");
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (towards) {
+			transform.position = Vector3.MoveTowards (transform.position, earth.transform.position, speed);
+			if(Vector3.Distance (transform.position, earth.transform.position) <= 0.5){
+			towards = false;
+			}
+		} else {
+			transform.position = Vector3.MoveTowards (transform.position, initialPosition, speed);
+			if (Vector3.Distance (transform.position, initialPosition) <= 0.5) {
+				towards = true;
+			}
+		}
+	}
+}
