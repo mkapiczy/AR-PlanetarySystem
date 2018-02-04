18b4d02d20a50f241bc39b14ecf9be76d9d90b74
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-04 16:28:51 +0100

    f) Earth and moon movement over ellipse set directly without planet hierarchy. TODO - rotation done manually

--- Assets/Scripts/EarthSpinner.cs
+++ Assets/Scripts/EarthSpinner.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthSpinner : MonoBehaviour {
-	public float spinSpeed;
-	public float orbitSpeed;
+	public float spinSpeed= 60f;
+	public float orbitSpeed = 0.01f;
 	private GameObject sunObject;
+	public float a = 9f;
+	public float b = 6f;
+	private float angle;
 
 	// Use this for initialization
 	void Start () {
 		sunObject = GameObject.Find("Sun");
+		angle = Mathf.Acos (transform.position.x / (a * 180));
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+		angle = angle + orbitSpeed;
 
-		transform.RotateAround (sunObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+		Vector3 newPosition = new Vector3 (a*Mathf.Cos(angle),transform.position.y,b*Mathf.Sin(angle));
+
+		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+		transform.position = newPosition;
 	}
+
 }
--- Assets/Scripts/MoonRotator.cs
+++ Assets/Scripts/MoonRotator.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class MoonRotator : MonoBehaviour {
 
-	public float spinSpeed;
-	public float orbitSpeed;
+	public float spinSpeed = 20f;
+	public float orbitSpeed = 0.002f;
+	public float a = 3f;
+	public float b = 2f;
+	private float angle;
 	private GameObject earthObject;
 
 
 	// Use this for initialization
 	void Start () {
 		earthObject = GameObject.Find("Earth");
+		angle = Mathf.Acos((transform.position.x - earthObject.transform.position.x) / (a * 180));
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
+		angle = angle + orbitSpeed;
+		Vector3 newPosition = new Vector3 (earthObject.transform.position.x + a*Mathf.Cos(angle),transform.position.y,earthObject.transform.position.z + b*Mathf.Sin(angle));
 		transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
-
-		transform.RotateAround (earthObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+		transform.position = newPosition;
 	}
 }
